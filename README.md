8bf7be03b09a3dc2fb6f554811ac5459be954175
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-10-06 13:06:24 -0400

    Added the knexfile and configured it a little bit, added more to the readme, created migrations

---
+++ README.md
@@ -0,0 +1,32 @@
+Steps to setup: 
+npm init -y
+npx gitignore node
+npm i -D nodemon
+npm i express helmet knex sqlite3
+touch index.js
+
+add scripts: 
+"server": "nodemon index.js"
+"start": "node index.js"
+
+Migrations record changes to the database structure over time, just klike git commits record changes to code
+
+configure knex connections
+- knex init 
+  - creates a knexfile.js
+  - luis removed the staging portion of the knex file 
+  - change the file name under development to the desired file name
+  - useNullAsDefault: true, only for sqlite
+
+commonly used migration commands: 
+- npx knex migrate:make <give it a name>...
+  - ex. knex migrate:make init
+    - uses development environment by default
+
+
+Project: Track Food AKA menu items
+- menu_items table: 
+  - id: primary key, unsigned (non-negative numbers) integer
+  - name: string, required (NOT NULL), unique, make searching by name fast
+  - price: floating point, not required
+  - available: boolean, default 'false' 
---
+++ knexfile.js
@@ -0,0 +1,50 @@
+// Update with your config settings.
+
+module.exports = {
+  // configuration for the development environment 
+  development: {
+    client: 'sqlite3',
+    connection: {
+      //filename: './dev.sqlite3'   // while file the code is using 
+      filename: './database/food.db3',
+    },
+    useNullAsDefault: true      // required for sqlite3 ONLY
+  },
+
+  // Luis deleted staging
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // configuration for rthe production environment db connection
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: 'postgresql://pgontheweb.com',
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
