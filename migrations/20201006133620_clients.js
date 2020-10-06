d0d64d2f4d493f9e52a29cb85dbbf985ad510d43
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-10-06 13:44:55 -0400

    Created a new table inside the food database called 'client'

---
+++ migrations/20201006133620_clients.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('clients', tbl => {
+    //tbl.string('id'),  tbl.primary('id');
+    tbl.string('id').primary();
+    
+    tbl.string('name').notNullable().index();
+    tbl.string('email').unique().index();
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('clients');  
+};
